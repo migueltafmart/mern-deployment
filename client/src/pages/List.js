dd061f7a75ab4abab6f75073f699859f5eb6ed3f
Miguel <migueltafmart@gmail.com>
2021-07-07 16:47:28 +0200

    Feat: Developed a whole entire React app

---
+++ client/src/pages/List.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class List extends Component {
+  // Initialize the state
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+  }
+
+  // Fetch the list on first mount
+  componentDidMount() {
+    this.getList();
+  }
+
+  // Retrieves the list of items from the Express app
+  getList = () => {
+    axios("/api/getList").then((list) => this.setState({ list: list.data }));
+  };
+
+  render() {
+    const { list } = this.state;
+
+    return (
+      <div className="App">
+        <h1>List of Items</h1>
+        {/* Check to see if any items are found*/}
+        {list.length ? (
+          <div>
+            {/* Render the list of items */}
+            {list.map((item, i) => {
+              return <div key={i} >{item}</div>;
+            })}
+          </div>
+        ) : (
+          <div>
+            <h2>No List Items Found</h2>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default List;
